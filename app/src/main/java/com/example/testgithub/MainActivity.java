ee0a639d9912cbd985d0195fd84d7f6c187d43f8
Mantosh-sys <85169050+Mantosh-sys@users.noreply.github.com>
2021-06-02 01:32:10 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/testgithub/MainActivity.java
+++ app/src/main/java/com/example/testgithub/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"Waoo",Toast.LENGTH_LONG).show();
+        
     }
 }
