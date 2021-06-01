7c270ce6378557d13f1e32752832574d7e6fb0de
Mantosh-sys <singhmantosh964@gmail,com>
2021-06-01 18:19:28 +0530

    SecondCommit..

--- app/src/main/java/com/example/testgithub/MainActivity.java
+++ app/src/main/java/com/example/testgithub/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
   String a = "Mantosh";
+  String b = "Shiva";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
